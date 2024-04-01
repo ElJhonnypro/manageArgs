2ad3c2059c604219d18b4bf77cf3a4a0a5341183
ElJhonnypro <tucorreo@gmail.com>
2024-04-01 18:16:42 -0500

    main.js no main.ts

---
+++ main.js
@@ -0,0 +1,24 @@
+function manageArgs(process, argscommands, log) {
+    var args = process.argv.slice(2);
+    if (args.length === 0) {
+        var func = argscommands["default"];
+        if (func === undefined) {
+            if (log) {
+                console.log("\x1b[1;31m" + "no default command put it will be the normal way");
+            }
+        }
+        else if (log) {
+            console.log("\x1b[1;32m" + "Default mode");
+        }
+    }
+    else {
+        if (args[0] in argscommands) {
+            var func = argscommands[args[0]];
+            func();
+        }
+        else {
+            console.log("Invalid command.");
+        }
+    }
+}
+module.exports = manageArgs;
